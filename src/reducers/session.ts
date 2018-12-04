328ed0b608456eec4bb7a1f18dd15f5a343d28ca
HJF <2283785225@qq.com>
2018-12-04 09:10:34 +0800

    刪除 獲取下級機構信息

--- src/reducers/session.ts
+++ src/reducers/session.ts
@@ -3,50 +3,35 @@ import { session as types } from '../constants/ActionTypes';
 
 const DEFAULT_STATE = {
   /**
-   * 判断是否跳转到登录
+   * 判断是否绑定
    */
-  hasLogin: false,
-  agencies: [],
-
-  childrenAgencies: [],
-  /**
-   * 下拉框选择时从服务端获取的资源信息
-   */
-  selectResource: []
+  hasBinding: false,
+  merchants: {
+    merchants: []
+  },
+  processing: false
 };
 
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2018/5/4
  */
 export default compose((state = DEFAULT_STATE, action) => {
-  const {payload, type, status, success} = action;
+  const {payload, type, success} = action;
   switch (type) {
     // 获取个人机构信息start
     case types.startProfile:
       return {
         ...state,
-        hasLogin: false
+        hasBinding: false,
+        processing: true
       };
     // 获取个人机构信息end
     case types.profileComplete: {
       return {
         ...state,
-        hasLogin: !success,
-        agencies: success && payload || state.agencies
-      }
-    }
-    // 获取下级机构
-    case types.getChildrenAgencyComplete: {
-      return {
-        ...state,
-        childrenAgencies: (success && status === '0000' && payload instanceof Object) ? payload : state.childrenAgencies
-      };
-    }
-    // 获取交易平台 资源列表
-    case types.getResourcesComplete: {
-      return {
-        ...state,
-        selectResource: success ? payload : []
+        processing: false,
+        hasBinding: success && payload.merchants.length > 0,
+        merchants: success && payload.merchants || state.merchants
       };
     }
     default:
