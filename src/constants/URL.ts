7c901c729849ae6f5ec6c1af03d95ba60a95772e
HJF <2283785225@qq.com>
2018-12-04 09:13:05 +0800

    接口配置

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -8,45 +8,32 @@ export {
  * @author 田尘殇Sean(sean.snow@live.com) createAt 2018/4/19
  */
 export default {
-  agency: `${api}/agencies`,
-  agencyBusiness: agencyId => `${api}/agencies/${agencyId}/business`,
+  session: `${api}/session`,
 
-  channel: `${api}/channels`,
-  channelBusiness: channelsFlag => `${api}/channels/${channelsFlag}/business`,
-  channelMerchant: `${api}/channels/merchants`,
-  channelConfig: channelFlag => `${api}/channels/${channelFlag}/configs`,
-  channelMerchantBusiness: businessType => `${api}/channels/merchants/${businessType}/businesses`,
+  loginBound: `${api}/session/operators/merchants`,
+  loginSend: `${api}/merchant-check-value`,
+  logout: `${authApi}/logout`,
+
+  paymentTrade: `${api}/payment-records`,
+  settlePayment: `${api}/settle`,
 
   merchant: `${api}/merchants`,
-  merchantBusiness: merchantId => `${api}/merchants/${merchantId}/business`,
 
-  paymentTrade: `${api}/payment/trades`,
-  paymentRoute: `${api}/payment/route-rules`,
+  qrCode: `${api}/qr-codes`,
+  spdQrCode: `${api}/qr-codes`,
+
+
   permissions: `${api}/permissions`,
-
-  rateTemplate: `${api}/template`,
-
-  settlePayment: `${api}/payment/trade-settle`,
-  settleTask: `${api}/payment/settle-task`,
-  settleDetails: `${api}/payment/settle-detail`,
-  settleReconciliationTask: `${api}/payment/reconciliation-task`,
-  settleCheckMistakes: `${api}/payment/mistakes`,
-
-  session: `${api}/session`,
-  logout: `${authApi}/logout`,
-  systemClient: `${api}/session/agencies/clients`,
-  loginBound: `${api}/session/operators/agencies`,
 
   systemResources: `${api}/permissions`,
   systemConfig: `${api}/config`,
   systemOperator: `${api}/operators`,
   systemProcess: `${api}/process`,
 
-  loginSend: `${api}/agency-check-value`,
   roles: `${api}/roles`,
 
   controlMerchantLimits: `${api}/payment-limits`,
 
-  paymentRiskRule:`${api}/paymentLimit`,
   advanceMoney:`${api}/overdraft-amounts`,
+
 }
