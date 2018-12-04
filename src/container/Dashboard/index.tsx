98bc6302ed5b62e1b30add23d87cbbefb80128e2
HJF <2283785225@qq.com>
2018-12-04 09:37:27 +0800

    獲取 統計信息

--- src/container/Dashboard/index.tsx
+++ src/container/Dashboard/index.tsx
@@ -4,14 +4,16 @@
  * date: 2018/11/22
  */
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { ReactNode } from 'react';
 import { ChartCard } from 'ant-design-pro/lib/Charts';
 import { IChartCardProps } from 'ant-design-pro/lib/Charts/ChartCard';
 import { Col, Icon, Row, Tooltip } from 'antd';
-
+import { autoBind } from '@sitb/wbs/autoBind';
 import { GrayBackgroundContainer } from '../../component/HocComponent/GrayBackgroundContainer';
 import { chartCardConfig } from './config';
 import { Content } from './Content';
+import { getActions } from '../../core/store';
 
 export interface ActionItem {
   title: string;
@@ -23,7 +25,21 @@ export interface ChartCardProps extends IChartCardProps {
   children?: ReactNode
 }
 
+
+@connect(({paymentTrade}) => ({
+  aliPay: paymentTrade.aliPay,
+  weChatPay: paymentTrade.weChatPay,
+  unionPay: paymentTrade.unionPay,
+  posPayDirect: paymentTrade.posPayDirect,
+  posPayIndirect: paymentTrade.posPayIndirect,
+  all: paymentTrade.all
+}))
+@autoBind
 export class Dashboard extends React.Component<any> {
+
+  componentWillMount() {
+    getActions().paymentTrade.searchStats();
+  }
 
   static title = '仪表盘';
 
