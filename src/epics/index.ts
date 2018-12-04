9a6cd4773db2a8ef99c32f491e3c2ccc43796b42
HJF <2283785225@qq.com>
2018-12-04 09:22:58 +0800

    刪除 無用模塊

--- src/epics/index.ts
+++ src/epics/index.ts
@@ -1,97 +1,50 @@
-import * as agency from './agency';
-import * as agencyConfig from './agencyConfig';
-import * as agencyBusiness from './agencyBusiness';
-import * as agencyProfit from './agencyProfit';
-
-import * as channel from './channel';
-import * as channelBusiness from './channelBusiness';
-import * as channelConfig from './channelConfig';
-import * as channelMerchant from './channelMerchant';
-import * as channelMerchantBusiness from './channelMerchantBusiness';
-
 import * as login from './login';
 
 import * as merchant from './merchant';
-import * as merchantBusiness from './merchantBusiness';
 import * as merchantReview from './merchantReview';
 import * as merchantBusinessReview from './merchantBusinessReview';
 
 import * as navigator from './navigator';
 
 import * as paymentTrade from './paymentTrade';
-import * as paymentRoute from './paymentRoute';
 import * as permissions from './permissions';
 
 import * as settlePayment from './settlePayment';
-import * as settleTask from './settleTask';
-import * as settleDetails from './settleDetails';
-import * as settleTradeDetails from './settleTradeDetails';
-import * as settleCheckMistake from './settleCheckMistake';
-import * as settleReconciliationTask from './settleReconciliationTask';
 
 import * as session from './session';
-import * as profile from './profile';
 import * as user from './user';
 
-import * as systemClient from './systemClient';
 import * as systemConfig from './systemConfig';
 import * as systemRole from './systemRole';
 import * as systemResource from './systemResource';
 import * as systemOperator from './systemOperator';
 import * as systemProcess from './systemProcess';
-import * as controlMerchantLimits from './controlMerchantLimits';
-
-import * as rateTemplate from './rateTemplate';
-import * as cushionQuotManagement from './cushionQuotManagement';
 
 /**
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2018/5/4
  */
 export {
-  agency,
-  agencyConfig,
-  agencyBusiness,
-  agencyProfit,
-
-  channel,
-  channelBusiness,
-  channelConfig,
-  channelMerchant,
-  channelMerchantBusiness,
 
   login,
   merchant,
-  merchantBusiness,
   merchantReview,
   merchantBusinessReview,
 
   navigator,
 
   paymentTrade,
-  paymentRoute,
   permissions,
 
   settlePayment,
-  settleTask,
-  settleDetails,
-  settleTradeDetails,
-  settleCheckMistake,
-  settleReconciliationTask,
 
   session,
-  profile,
   user,
 
   systemRole,
   systemResource,
   systemConfig,
   systemOperator,
-  systemClient,
   systemProcess,
 
-  controlMerchantLimits,
-
-  rateTemplate,
-  cushionQuotManagement,
 }
