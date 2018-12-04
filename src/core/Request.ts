d5bb2c20b956fcef7da703dfd10ed8e2eee8a8a1
HJF <2283785225@qq.com>
2018-12-04 09:09:30 +0800

    刪除用戶授權信息

--- src/core/Request.ts
+++ src/core/Request.ts
@@ -4,14 +4,11 @@
  * date: 2018/4/25
  */
 import Request, { Params } from '@sitb/wbs/utils/Request';
-import { getAccessToken, getAgencyId } from './SessionServices';
 import { queryToObjectWithUrl, urlArgs } from '@sitb/wbs/utils/HttpUtil';
 
 export async function execute({headers, url, method, ...args}: Params) {
   const newHeaders: any = {
-    ...headers,
-    authorization: `Bearer ${getAccessToken()}`,
-    ['X-Agency-ID']: getAgencyId()
+    ...headers
   };
   let newUrl = url;
   let newArgs: any = Object.assign({}, args);
